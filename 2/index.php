7234c8d43198476bf4d68f1ce2ab6aa6942d3daf
Danil Barkov <55778811+danil-11906@users.noreply.github.com>
2021-03-09 17:47:46 +0300

    Update index.php

--- 2/index.php
+++ 2/index.php
@@ -3,35 +3,39 @@ include_once('Task.html');
 $textarea = $_REQUEST['textarea'];
 if (!empty($textarea)) {
     $num = 0;
+    $array = "";
+    $call = 0;
+    $text = $textarea;
     function generate($text)
     {
-        $call = 0;
         for ($i = 0; $i < strlen($text); $i++) {
             switch ($text[$i]){
                 case "h":
                     $text[$i] = 4;
-                    $call++;
                     break;
                 case "l":
                     $text[$i] = 1;
-                    $call++;
                     break;
                 case "e":
                     $text[$i] = 3;
-                    $call++;
                     break;
                 case "o":
                     $text[$i] = 0;
-                    $call++;
                     break;
             }
             yield $text[$i];
         }
-        echo "<br />".$call;
     }
     foreach (generate($textarea) as $val) {
-        echo $val[$num];
+        $array .= $val[$num];
     }
+    for ($i = 0; $i < strlen($textarea); $i++) {
+        if ($array[$i] != $text[$i]) {
+            $call++;
+        }
+    }
+    echo $array;
+    echo "<br />".$call;
 }
 else{
     echo "Введите значение";
